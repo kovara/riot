29d6d64e60ff6643083feef05ced54882759ad7c
Rex Belli <rexbelli@gmail.com>
2017-03-01 08:37:24 -0500

    new commit

--- tcpi.go
+++ tcpi.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import ("fmt"
+	"log"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+	"time"
+)
+
+var (
+	device		string = "ens32"
+	snapshot_len	int32 = 1024
+	promiscuous	bool = false
+	err		error
+	timeout		time.Duration = 3 * time.Second
+	handle		*pcap.Handle
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
+	if err != nil {log.Fatal(err)}
+	defer handle.Close()
+
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		fmt.Println(packet)
+		break
+	}
+
 }
