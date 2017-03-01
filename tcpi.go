40c2e51f315923f94bf8ce6928493818f409cfa9
Rex Belli <rexbelli@gmail.com>
2017-03-01 09:55:49 -0500

    commit

--- tcpi.go
+++ tcpi.go
@@ -1,6 +1,7 @@
 package main
 
-import ("fmt"
+import ("flag"
+	"fmt"
 	"log"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
@@ -10,21 +11,34 @@ import ("fmt"
 var (
 	device		string = "ens32"
 	snapshot_len	int32 = 1024
-	promiscuous	bool = false
+	promiscuous	bool = true
 	err		error
-	timeout		time.Duration = 3 * time.Second
+	timeout		time.Duration = -1 * time.Second
 	handle		*pcap.Handle
 )
 
+func printPacketInfo(packet gopacket.Packet) {
+}
+
 func main() {
+	clientPtr := flag.String("client", "", "IP of client")
+	serverPtr := flag.String("server", "", "IP of server")
+	arpPtr := flag.Bool("noarp",false,"Do not Arpspoof")
+	flag.Parse()
+	if len(*clientPtr) == 0 {log.Fatal("no client IP address entered")}
+	if len(*serverPtr) == 0 {log.Fatal("no server IP address entered")}
+	if *arpPtr == true {log.Fatal("arp flag")}
+
 	handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
+	if err != nil {log.Fatal(err)}
+	err = handle.SetBPFFilter("host "+*clientPtr+" and host "+*serverPtr)
 	if err != nil {log.Fatal(err)}
 	defer handle.Close()
 
+	fmt.Println("capturing...")
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
 		fmt.Println(packet)
-		break
 	}
 
 }
